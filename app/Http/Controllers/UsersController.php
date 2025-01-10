aeeba90883beb329501cd7562b42247d9bd49ae2
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2025-01-10 09:55:55 +0100

    Initial commit, fixed user edition

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserUpdateRequest;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\RedirectResponse;
+
+class UsersController extends Controller
+{
+    /**
+     * Display the active users.
+     */
+    public function index(Request $request): Response
+    {
+        return Inertia::render('Users/Index', [
+            'users' => User::all(),
+        ]);
+    }
+
+    /**
+     * Edit the user account.
+     */
+    public function edit(Request $request): Response
+    {
+        return Inertia::render('Users/Edit', [
+            'user' => User::find($request->user),
+        ]);
+    }
+
+    /**
+     * Update the user information.
+     */
+    public function update(UserUpdateRequest $request): RedirectResponse
+    {
+        // Removes password field if it's null
+        if (!$request->password) {
+            unset($request['password']);
+        }
+
+        // Update the User details
+        User::find($request->user)->update($request->all());
+
+        // Redirect to the User Index page
+        return Redirect::route('users.index');
+    }
+
+     /**
+   * Delete the user account.
+   */
+  public function delete(Request $request): RedirectResponse
+  {
+    User::find($request->user)->delete();
+
+    return Redirect::route('users.index');
+  }
+
+    /**
+     * Create the user account.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Users/Create');
+    }
+
+    /**
+     * Store the user account.
+     */
+    public function store(UserUpdateRequest $request): RedirectResponse
+    {
+        // Store the User details
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return Redirect::route('users.index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+// Users CRUD Controller
+use App\Http\Controllers\UsersController;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Users CRUD routes
+    Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+    Route::get('/users/{user}/edit', [UsersController::class, 'edit'])->name('users.edit');
+    Route::patch('/users/{user}/update', [UsersController::class, 'update'])->name('users.update');
+    Route::delete('/users/delete', [UsersController::class, 'delete'])->name('users.delete');
+    Route::get('/users/create', [UsersController::class, 'create'])->name('users.create');
+    Route::post('/users/store', [UsersController::class, 'store'])->name('users.store');
+});
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
